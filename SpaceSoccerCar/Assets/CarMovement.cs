503464100162e8c1330b7afae77ad5b5df4afb77
Marcos José Cecotti <marcosjcecotti@hotmail.com>
2022-05-11 23:49:12 -0300

    Cambie el script, edite el layout order y lo frize en z

--- SpaceSoccerCar/Assets/CarMovement.cs
+++ SpaceSoccerCar/Assets/CarMovement.cs
@@ -7,30 +7,42 @@ public class CarMovement : MonoBehaviour
     WheelJoint2D wheel1;
     WheelJoint2D wheel2;
     JointMotor2D motor;
+    public float runSpeed;
+    public float walkSpeed;
+    public float maxSpeed = 500;
+    Rigidbody2D rb;
     void Start()
     {
         wheel1 = gameObject.GetComponents<WheelJoint2D>()[0];
         wheel2 = gameObject.GetComponents<WheelJoint2D>()[1];
         motor = new JointMotor2D();
+        runSpeed = 5;
+        walkSpeed = 0;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.RightArrow))
-        {
-            motor.motorSpeed = -800;
-            motor.maxMotorTorque = 1000;
-            wheel1.useMotor = true;
-            wheel1.motor = motor;
-            wheel2.useMotor = true;
-            wheel2.motor = motor;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            wheel1.useMotor = false;
-            wheel2.useMotor = false;
+        float input = Input.GetAxis("Horizontal");
 
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (runSpeed < maxSpeed)
+            {
+                runSpeed += Time.deltaTime;
+            }
+
+            rb.velocity = new Vector2(input * runSpeed, rb.velocity.y);
         }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            runSpeed = 5;
+        }
+        else
+        {
+            rb.velocity = new Vector2(input * walkSpeed, rb.velocity.y);
+        }
+
     }
 }
